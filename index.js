a61491fbc72db96dc5e192c9499f9d552c2f3111
Monica Perez-Valle <monicaperez-valle@pursuit.org>
2023-04-27 12:11:21 -0400

    myIntro

--- index.js
+++ index.js
@@ -3,14 +3,19 @@ console.log("Hey you! Yeah you! Awesome Developer! You're doing Great Fam! 💪�
 
 
 const meObj = {
-    name: "Sam",
-    hometown: "The Boogie-Down - BX 🙅🏾‍♂️",
-    favoriteColors: ["gold", "navy blue", "forest green", "red"]
+    name: "Monica Perez-Valle",
+    hometown: "Money Earnin' Mount Vernon🤪",
+    favoriteColors: ["red", "orange", "yellow", "green", "BLUE!", "indigo", "violet"]
 }
 
 
 function introduceMe (personObj){
+    console.log(`Hi, my name is ${personObj.name}, and I'm from ${personObj.hometown}.`)
+
+    console.log(`My favorite colors are ${personObj.favoriteColors}`)
     // print out: "Hi, my name is {your name}, and I'm from {your hometown}"
 
     // print out: "My favorite colors are {your favorite colors}"
 }
+
+introduceMe(meObj)
